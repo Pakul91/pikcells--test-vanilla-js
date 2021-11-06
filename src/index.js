467052b1e9dfc245b77d8ca35ff29840f2c1ea80
Tomasz <pakulniewicz.t@gmail.com>
2021-11-06 17:11:47 +0000

    assign img src on load added

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import { state } from "./state";
 
 const designDisplay = document.querySelector("desigin-display-container");
 const layers = document.querySelectorAll(".layer");
-const images = document.querySelectorAll(".img");
-const items = document.querySelectorAll(".item");
+let items = "";
 
 /**
  *
@@ -14,20 +13,21 @@ function populateLayers() {
   layers.forEach((layer) => {
     const order = layer.dataset.layer;
     const markup = state.layers[order].items
-      .map((item) => itemMarkup(item))
+      .map((item) => itemMarkup(item, order))
       .join("");
 
     layer.insertAdjacentHTML("beforeend", markup);
   });
+  items = document.querySelectorAll(".item");
 }
 
-function itemMarkup(item) {
+function itemMarkup(item, order) {
   if (!item) return;
   return `
     <p class='item ${item.order === 0 ? "active" : ""}' 
-        data-img-src="https://lab.pikcells.com/code-exercise/images/${
+        data-imgsrc="https://lab.pikcells.com/code-exercise/images/${
           item.imgSrc
-        }">
+        }" data-layer=${order}>
         ${item.name}
     </p> 
     `;
@@ -37,8 +37,19 @@ function assignImgSrc(layer, src) {
   document.querySelector(`.img-layer${layer}`).setAttribute("src", `${src}`);
 }
 
+function assignImgSrcOnLoad() {
+  if (!items) return;
+  items.forEach((item) => {
+    if (item.classList.contains("active")) {
+      console.log(item.dataset.layer, item.dataset);
+      assignImgSrc(item.dataset.layer, item.dataset.imgsrc);
+    }
+  });
+}
+
 function initialise() {
   populateLayers();
+  assignImgSrcOnLoad();
 }
 
 initialise();

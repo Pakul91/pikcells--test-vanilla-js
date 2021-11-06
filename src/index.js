778b56a4f88317c03eef93b5c05784d92b11a838
Tomasz <pakulniewicz.t@gmail.com>
2021-11-06 16:41:26 +0000

    state sorted and layers populated

--- src/index.js
+++ src/index.js
@@ -1,9 +1,44 @@
 import { state } from "./state";
-const designContainer = document.querySelector(".design-selection-container");
-const layers = document.querySelectorAll(".layer");
 
+const designDisplay = document.querySelector("desigin-display-container");
+const layers = document.querySelectorAll(".layer");
+const images = document.querySelectorAll(".img");
+const items = document.querySelectorAll(".item");
+
+/**
+ *
+ */
 function populateLayers() {
-  console.log(state);
+  if (!state || state === {}) return;
+
+  layers.forEach((layer) => {
+    const order = layer.dataset.layer;
+    const markup = state.layers[order].items
+      .map((item) => itemMarkup(item))
+      .join("");
+
+    layer.insertAdjacentHTML("beforeend", markup);
+  });
 }
 
-populateLayers();
+function itemMarkup(item) {
+  if (!item) return;
+  return `
+    <p class='item ${item.order === 0 ? "active" : ""}' 
+        data-img-src="https://lab.pikcells.com/code-exercise/images/${
+          item.imgSrc
+        }">
+        ${item.name}
+    </p> 
+    `;
+}
+
+function assignImgSrc(layer, src) {
+  document.querySelector(`.img-layer${layer}`).setAttribute("src", `${src}`);
+}
+
+function initialise() {
+  populateLayers();
+}
+
+initialise();

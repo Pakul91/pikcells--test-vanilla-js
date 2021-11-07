1d05f73db2c2f0ce330e478f58ea37ad94e15173
Tomasz <pakulniewicz.t@gmail.com>
2021-11-07 03:13:46 +0000

    replacing images with canvas

--- src/index.js
+++ src/index.js
@@ -1,54 +1,121 @@
-import { state } from "./state";
+import { fetchData, state } from "./state";
+import "./style.css";
 
-const designDisplay = document.querySelector("desigin-display-container");
-const layers = document.querySelectorAll(".layer");
+const layers = document.querySelectorAll(".layer-container");
+const newItemsBtn = document.querySelector(".new-items");
 let items = "";
 
 /**
- *
+ * Take data from te state and populate each layer with appropriate items
+ * @param {{}} state data retrived from server
  */
-function populateLayers() {
-  if (!state || state === {}) return;
+function populateLayers(state) {
+  if (Object.keys(state).length === 0) return;
 
+  const defConfig = state.default_configuration;
+
+  console.log(defConfig);
+
+  //take each layer and create markup for every item in it.
   layers.forEach((layer) => {
-    const order = layer.dataset.layer;
-    const markup = state.layers[order].items
-      .map((item) => itemMarkup(item, order))
+    const layerNr = layer.dataset.layer;
+
+    const markup = state.layers[layerNr].items
+      .map((item, i) => itemMarkup(item, layerNr, i, defConfig[layerNr]))
       .join("");
 
     layer.insertAdjacentHTML("beforeend", markup);
   });
+  // assign newly created items to global variable
   items = document.querySelectorAll(".item");
 }
 
-function itemMarkup(item, order) {
+/**
+ *
+ * @param {{}} item object containing data for each item
+ * @param {number} layerNr defines which layer the item is assigned to
+ * @param {*} i index nr of the item in the array.
+ * @returns template literal containing markup for every item
+ */
+function itemMarkup(item, layerNr, i, defConfig) {
+  console.log(defConfig);
   if (!item) return;
   return `
-    <p class='item ${item.order === 0 ? "active" : ""}' 
+    <p class='item btn ${i === defConfig ? "active" : ""}' 
         data-imgsrc="https://lab.pikcells.com/code-exercise/images/${
           item.imgSrc
-        }" data-layer=${order}>
+        }" data-layer=${layerNr}>
         ${item.name}
     </p> 
     `;
 }
 
+/**
+ *
+ * @param {number} layer dictates which layer img to select
+ * @param {string} src string to be passed intu img src attrobute
+ */
 function assignImgSrc(layer, src) {
   document.querySelector(`.img-layer${layer}`).setAttribute("src", `${src}`);
 }
 
+/**
+ * assign img src to each layer img from coresponding active layer items
+ */
 function assignImgSrcOnLoad() {
   if (!items) return;
   items.forEach((item) => {
     if (item.classList.contains("active")) {
-      console.log(item.dataset.layer, item.dataset);
       assignImgSrc(item.dataset.layer, item.dataset.imgsrc);
     }
   });
 }
 
-function initialise() {
-  populateLayers();
+/**
+ * Callback function for clicking on each item in each Layer
+ * @param {{}} event
+ * @returns
+ */
+function handleItemClick(event) {
+  const clicked = event.target.closest(".item");
+
+  if (!clicked) return;
+
+  //extract data from clicked item
+  const { imgsrc, layer } = clicked.dataset;
+  //check if item is already active
+  if (!clicked.classList.contains("active")) {
+    //remove active class from all elements in given layer
+    document
+      .querySelectorAll(`[data-layer="${layer}"]`)
+      .forEach((element) => element.classList.remove("active"));
+    //apply active class to clicked element
+    clicked.classList.add("active");
+    // assign img src from clicked item data
+    assignImgSrc(layer, imgsrc);
+  }
+}
+
+/**
+ * Callback function for clinking on new items btn
+ */
+function handleNewItemsBtnClick() {
+  //clear each layer
+  layers.forEach((layer) => {
+    layer.innerHTML = "";
+  });
+  initialise();
+}
+
+layers.forEach((layer) => {
+  layer.addEventListener("click", handleItemClick);
+});
+
+newItemsBtn.addEventListener("click", handleNewItemsBtnClick);
+
+async function initialise() {
+  await fetchData();
+  populateLayers(state);
   assignImgSrcOnLoad();
 }
 
--- src/state.js
+++ src/state.js
@@ -9,74 +9,17 @@ export let state = {};
  * fetch data from the server
  * @returns {} with organised data
  */
-function fetchData() {
-  const data = {
-    layers: [
-      {
-        order: 0,
-        items: [
-          {
-            order: 1,
-            name: "Almost Silly Silver",
-            imgSrc: "BWK.jpg",
-          },
-          {
-            order: 0,
-            name: "Typically Pretty Blue",
-            imgSrc: "hk9.jpg",
-          },
-          {
-            order: 2,
-            name: "Terribly Honest Black",
-            imgSrc: "aDn.jpg",
-          },
-        ],
-      },
-      {
-        order: 2,
-        items: [
-          {
-            order: 2,
-            name: "Too Melodic Silver",
-            imgSrc: "jeb.png",
-          },
-          {
-            order: 0,
-            name: "Never Generous Silver",
-            imgSrc: "VRC.png",
-          },
-          {
-            order: 1,
-            name: "Terribly Juicy Green",
-            imgSrc: "58Z.png",
-          },
-        ],
-      },
-      {
-        order: 1,
-        items: [
-          {
-            order: 0,
-            name: "Really Eloquent Blue",
-            imgSrc: "0Og.png",
-          },
-          {
-            order: 1,
-            name: "Extremely Honest Silver",
-            imgSrc: "2Ks.png",
-          },
-          {
-            order: 2,
-            name: "Terribly Confident Pink",
-            imgSrc: "L99.png",
-          },
-        ],
-      },
-    ],
-    default_configuration: [2, 1, 0],
-  };
+export async function fetchData() {
+  const url = " https://lab.pikcells.com/code-exercise/data.json";
 
-  state = sortData(data);
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    state = sortData(data);
+  } catch (e) {
+    state = { error: e.message };
+  }
 }
 
 /**
@@ -100,8 +43,6 @@ function sortData(input) {
   if (!input) return;
 
   const data = { ...input };
-  //sort layers in ascending order
-  data.layers.sort(sortByOrderProp);
 
   //sort items in each layer in ascending order
   data.layers.forEach((layer) => {
